173ee167d325032d019f1cb472c4c0ea8ed73db2
Usbak23 <amubarok1998@gmail.com>
2023-10-21 13:28:24 +0700

    Read Data

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,18 +1,32 @@
-module.exports={
-    index: async(req, res)=>{
+const Category = require("./model");
+
+module.exports = {
+    index: async (req, res) => {
         try {
-            res.render('admin/category/view_category'
-            )
+            const category = await Category.find();
+            res.render("admin/category/view_category", {
+                category
+            });
         } catch (error) {
             console.log(err);
-            
         }
     },
-    viewCreate : async(req, res)=>{
+    viewCreate: async (req, res) => {
         try {
-            res.render('admin/category/create')
+            res.render("admin/category/create");
         } catch (err) {
             console.log(err);
         }
-    }
-}
+    },
+    actionCreate: async (req, res) => {
+        try {
+            const { name } = req.body;
+            let category = await Category({ name });
+            await category.save();
+
+            res.redirect("/category");
+        } catch (err) {
+            console.log(err);
+        }
+    },
+};
