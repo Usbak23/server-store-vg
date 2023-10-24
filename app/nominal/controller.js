6e277fb09bf47417bf43a49067a5c77fec6a5d6f
Usbak23 <amubarok1998@gmail.com>
2023-10-24 10:42:21 +0700

    update fitur & nominal

--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -1,15 +1,110 @@
-const Nominal = require("./model")
+const Nominal = require("./model");
 
 module.exports = {
-    index: async(req, res) =>{
+    index: async (req, res) => {
         try {
-            const alertMessage = req.flash("alertMessage")
-            const alertStatus = req.flash("alertStatus")
+            const alertMessage = req.flash("alertMessage");
+            const alertStatus = req.flash("alertStatus");
 
-            const alert = { message: alertMessage, status: alertStatus}
-            const nominal = await Nominal.find()
+            const alert = { message: alertMessage, status: alertStatus };
+            const nominal = await Nominal.find();
+
+            console.log("alert");
+            console.log(alert);
+
+            res.render("admin/nominal/view_nominal", {
+                nominal,
+                alert,
+            });
         } catch (err) {
-            console.log(err);
+            req.flash("alertMessage", `${err.message}`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/nominal");
         }
-    }
-}
+    },
+
+    viewCreate: async (req, res) => {
+        try {
+            res.render("admin/nominal/create");
+        } catch (err) {
+            req.flash("alertMessage", `${err.message}`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/nominal");
+        }
+    },
+
+    actionCreate: async (req, res) => {
+        try {
+            const { coinName, coinQuantity, price } = req.body;
+            let nominal = await Nominal({ coinName, coinQuantity, price });
+            await nominal.save();
+
+            req.flash("alertMessage", "Berhasil Tambah Nominal");
+            req.flash("alertStatus", "success");
+
+            res.redirect("/nominal");
+        } catch (err) {
+            req.flash("alertMessage", `${err.message}`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/nominal");
+        }
+    },
+
+    viewEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const nominal = await Nominal.findOne({ _id: id });
+
+            res.render("admin/nominal/edit", {
+                nominal,
+            });
+        } catch (err) {
+            req.flash("alertMessage", `${err.message}`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/nominal");
+        }
+    },
+
+    actionEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { coinName, coinQuantity, price } = req.body;
+
+            Nominal.findOneAndUpdate(
+                {
+                    _id: id,
+                },
+                { coinName, coinQuantity, price }
+            );
+
+            req.flash('alertMessage', "Berhasil ubah nominal")
+            req.flash('alertStatus', "success")
+
+            res.redirect("/nominal");
+        } catch (err) {
+            req.flash("alertMessage", `${err.message}`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/nominal");
+        }
+    },
+
+    actionDelete: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            await Nominal.findOneAndRemove({
+                _id: id,
+            });
+
+            req.flash("alertMessage", "Berhasil Hapus Nominal");
+            req.flash("alertStatus", "success");
+
+            res.redirect("/nominal");
+        } catch (err) {
+            req.flash("alertMessage", `${err.message}`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/nominal");
+        }
+    },
+};
--- app/nominal/router.js
+++ app/nominal/router.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const {index} = require ('./controller')
+var express = require('express');
+var router = express.Router();
+const { index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete } = require('./controller')
 
+/* GET home page. */
 router.get('/', index);
+router.get('/create', viewCreate);
+router.post('/create', actionCreate);
+router.get('/edit/:id', viewEdit);
+router.put('/edit/:id', actionEdit );
+router.delete('/delete/:id', actionDelete)
+
+module.exports = router;
