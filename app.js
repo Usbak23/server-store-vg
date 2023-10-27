57d8a45cd6d4879273f1a307f089178c1cedd4c6
Usbak23 <amubarok1998@gmail.com>
2023-10-28 00:04:56 +0700

    create fiture transaction

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const nominalRouter = require("./app/nominal/router");
 const voucherRouter = require("./app/voucher/router");
 const bankRouter = require("./app/bank/router");
 const paymentRouter = require("./app/payment/router");
+const usersRouter = require("./app/users/router");
+const transactionRouter = require("./app/transaction/router");
 
 const app = express();
 
@@ -42,12 +44,17 @@ app.use(
 );
 
 // routing
-app.use("/", dashboardRouter);
+app.use("/", usersRouter);
+app.use("/dashboard", dashboardRouter);
 app.use("/category", categoryRouter);
 app.use("/nominal", nominalRouter);
 app.use("/voucher", voucherRouter);
 app.use("/bank", bankRouter);
 app.use("/payment", paymentRouter);
+app.use("/transaction", transactionRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
